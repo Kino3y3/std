917071162fa0856fefefe4e284e692e310e54fd5
martin <kino3y3@gmail.com>
2020-12-02 15:26:58 +0900

    update

--- react/webgame/012-wordrelay-webpack/WordRelay.jsx
+++ react/webgame/012-wordrelay-webpack/WordRelay.jsx
@@ -3,12 +3,52 @@ const { Component } = React;
 
 class WordRelay extends Component {
   state = {
-    text: 'hello, word',
+    word: '하이퍼',
+    value: '',
+    result: '',
+  };
+
+  onSubmitForm = (e) => {
+    e.preventDefault();
+    if (this.state.word[this.state.word.length - 1] === this.state.value[0]) {
+      this.setState({
+        result: 'correct!',
+        word: this.state.value,
+        value: '',
+      });
+      this.input.focus();
+    } else {
+      this.setState( {
+        result: 'incorrect!',
+        value: '',
+      });
+      this.input.focus();
+    }
+  };
+
+  onChangeInput = (e) => {
+    this.setState({ value: e.target.value });
+  };
+
+  input;
+
+  onRefInput = (c) => {
+    this.input = c;
   };
 
   render() {
     return (
-      <h1>{this.state.text}</h1>
+      <>
+        <div>{this.state.word}</div>
+        <form onSubmit={this.onSubmitForm}>
+          <input ref={this.onRefInput} value={this.state.value} onChange={this.onChangeInput} />
+          {/*
+          <input ref={this.onRefInput} defaultValue={this.state.value} />
+          */}
+          <button>Submit!</button>
+        </form>
+        <div>{this.state.result}</div>
+      </>
     )
   }
 }
